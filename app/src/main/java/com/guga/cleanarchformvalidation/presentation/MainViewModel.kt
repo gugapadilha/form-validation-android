e5c9d06df6e5f5edb5b2cfa1b04879cc18a75d6b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-11-02 17:35:45 -0300

    creating variables to pass the actuall content of the use cases created before

--- app/src/main/java/com/guga/cleanarchformvalidation/presentation/MainViewModel.kt
+++ app/src/main/java/com/guga/cleanarchformvalidation/presentation/MainViewModel.kt
@@ -4,8 +4,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.guga.cleanarchformvalidation.domain.use_cases.ValidateEmail
+import com.guga.cleanarchformvalidation.domain.use_cases.ValidatePassword
+import com.guga.cleanarchformvalidation.domain.use_cases.ValidateRepeatedPassword
+import com.guga.cleanarchformvalidation.domain.use_cases.ValidateTerms
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val validateEmail: ValidateEmail = ValidateEmail(),
+    private val validatePassword: ValidatePassword = ValidatePassword(),
+    private val validateRepeatedPassword: ValidateRepeatedPassword = ValidateRepeatedPassword(),
+    private val validateTerms : ValidateTerms = ValidateTerms()
+) : ViewModel() {
 
     var state by mutableStateOf(RegistrationFormState())
 
