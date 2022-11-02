a28cc058fc85676ac3d14297d7629bf38ad2e7b9
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-11-02 17:31:19 -0300

    validate terms done

---
+++ app/src/main/java/com/guga/cleanarchformvalidation/domain/use_cases/ValidateTerms.kt
@@ -0,0 +1,19 @@
+package com.guga.cleanarchformvalidation.domain.use_cases
+
+import android.util.Patterns
+
+class ValidateTerms {
+
+    fun execute(acceptedTerms: Boolean) : ValidationResult{
+        if (!acceptedTerms){
+            return ValidationResult(
+                successful = false,
+                errorMessage = "Please accept the terms"
+            )
+        }
+        return ValidationResult(
+            successful = true
+        )
+    }
+
+}
