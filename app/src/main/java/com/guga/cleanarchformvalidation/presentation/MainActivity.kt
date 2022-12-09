887da53078bc376555bc5ce490f3df3ba1527728
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-12-08 22:10:58 -0300

    mainViewModel instantiate on MainActivity

--- app/src/main/java/com/guga/cleanarchformvalidation/presentation/MainActivity.kt
+++ app/src/main/java/com/guga/cleanarchformvalidation/presentation/MainActivity.kt
@@ -3,13 +3,19 @@ package com.guga.cleanarchformvalidation.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.guga.cleanarchformvalidation.ui.theme.CleanArchFormValidationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val viewModel = viewModel<MainViewModel>()
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(32.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+
+                    }
                 }
             }
         }
